51617db582d26721323c1bba9944ee717e5fa883
TommasoGalistu <165350030+TommasoGalistu@users.noreply.github.com>
2024-09-19 10:49:02 +0200

    feat: form delete in index

--- resources/views/beers/index.blade.php
+++ resources/views/beers/index.blade.php
@@ -30,9 +30,12 @@
                         <a href="{{route('beers.edit', $beer)}}" class="btn btn-warning">
                             <i class="fa-solid fa-pencil"></i>
                         </a>
-                        <a href="#" class="btn btn-danger">
-                            <i class="fa-solid fa-trash"></i>
-                        </a>
+
+                        <form class="d-inline" action="{{ route('beers.destroy', $beer) }}" method="POST" onsubmit="return confirm('Sei sicuro di voler eliminare: {{ $beer->name }}')">
+                            @csrf
+                            @method('DELETE')
+                            <button class="btn btn-danger" type="submit"><i class="fa-solid fa-trash"></i></button>
+                        </form>
                     </td>
                 </tr>
 
